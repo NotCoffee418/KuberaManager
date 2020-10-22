0df0a6f030d9f248b22106dc5cb7a429c74c4ee2
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-10-22 04:02:50 +0200

    Half finished config editor in website
    - scrapped SetupController

---
+++ KuberaManager/KuberaManager/Controllers/ConfigController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using KuberaManager.Models.Database;
+
+namespace KuberaManager.Controllers
+{
+    public class ConfigController : Controller
+    {
+        public IActionResult Index()
+        {
+            var context = new kuberaDbContext();
+            ViewData.Model = context.Configs;
+            return View();
+        }
+
+        public IActionResult Edit(string confKey)
+        {
+            var tehfuc = Config.Get<string>(confKey);
+            return View(tehfuc);
+        }
+    }
+}
