dbbbc37c87aeb3882d7fd5f8b121f99a9c034862
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-10-23 20:51:18 +0200

    Completed /config edit forms

--- KuberaManager/KuberaManager/Controllers/ConfigController.cs
+++ KuberaManager/KuberaManager/Controllers/ConfigController.cs
@@ -17,10 +17,48 @@ namespace KuberaManager.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Edit(Config config)
+        {
+            try
+            {
+                // handle unusual config keys
+                switch (config.ConfKey)
+                {
+                    case "InstalledConfigVersion":
+                        ViewBag.Notification = "InstalledConfigVersion cannot be manually updated. No changes were saved.";
+                        break;
+
+                    default: // Update value in db
+                        Config.Set<string>(config.ConfKey, config.ConfValue);
+                        break;
+                }
+
+                // Retrieve current values from DB
+                ViewBag.ConfKey = config.ConfKey;
+                ViewBag.ConfValue = Config.Get<string>(config.ConfKey);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Notification = "Failed to update config value because: " + ex.Message;
+            }
+
+
+            return View(config);
+        }
+
+        [HttpGet("Config/Edit/{confKey}")]
         public IActionResult Edit(string confKey)
         {
-            var tehfuc = Config.Get<string>(confKey);
-            return View(tehfuc);
+            string confValue = Config.Get<string>(confKey);
+            Config newConf = new Config()
+            {
+                ConfKey = confKey,
+                ConfValue = confValue
+            };
+
+            return View(newConf);
         }
+
     }
 }
