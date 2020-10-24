218cc0961482280cff78545c09073fe9051b5041
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-10-24 05:27:00 +0200

    DiscordHandler implemented

--- KuberaManager/KuberaManager/Models/Logic/DiscordHandler.cs
+++ KuberaManager/KuberaManager/Models/Logic/DiscordHandler.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using KuberaManager.Models.Database;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace KuberaManager.Models.Logic
 {
     public class DiscordHandler
     {
-        public static void PostMessage(string msg)
+        static HttpClient client = new HttpClient();
+
+        public static void PostMessage(string msg) => PostMessageTask(msg).GetAwaiter().GetResult();
+
+        private static async Task PostMessageTask(string msg)
         {
-            throw new NotImplementedException();
+            string webhookToken = Config.Get<string>("DiscordApiKey");
+            string json = JsonConvert.SerializeObject(new Dictionary<string, string>
+            {
+                { "content", msg },
+            });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://discordapp.com/api/webhooks/" + webhookToken, content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
         }
     }
 }
