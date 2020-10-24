e4315dfd89cb0b2113a49ff2d8c6611208180754
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-10-25 00:33:01 +0200

    Computer.ByHostName() and .GetTag() implemented

--- KuberaManager/KuberaManager/Models/Database/Computer.cs
+++ KuberaManager/KuberaManager/Models/Database/Computer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KuberaManager.Models.Logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -35,7 +36,42 @@ namespace KuberaManager.Models.Database
 
         public static Computer ByHostname(string hostName)
         {
-            throw new NotImplementedException();
+            // Try to return a known computer
+            Computer comp = null;
+            using (var db = new kuberaDbContext())
+            {
+                comp = db.Computers
+                    .Where(x => x.Hostname == hostName)
+                    .FirstOrDefault();
+                if (comp != null)
+                    return comp;
+                else
+                {
+                    /// Computer didn't exist. Creating it.
+                    comp = new Computer()
+                    {
+                        Hostname = hostName
+                    };
+                    db.Computers.Add(comp);
+                    db.SaveChanges();
+                }
+            }
+
+            // Send discord notification if this computer is new
+            DiscordHandler.PostMessage(
+                $"New computer '{hostName}' has been added to the database. Change the config in KuberaManager as needed.");
+            return comp;
+        }
+
+        /// <summary>
+        /// Get the computer's current unique indentifier
+        /// </summary>
+        /// <returns></returns>
+        public string GetTag()
+        {
+            return ClientManager.GetConnectedComputers()
+                .Where(x => x.Value.host == Hostname)
+                .FirstOrDefault().Key;
         }
     }
 }
