8a911b9b5e130936be9aa3bd2800d13f939bcfd1
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-10-27 04:34:19 +0100

    Design: Set active link correctly
    - js handles tree menus (opens only if link inside is open)
    Bless random 0 upvotes answer on stackoverflow

--- KuberaManager/wwwroot/js/site.js
+++ KuberaManager/wwwroot/js/site.js
@@ -1,4 +1,23 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+// Opens treeview if a link inside it is the active link
+$(document).ready(() => {
+    // Check every relevant nav
+    $(function () {
+        var url = window.location;
+        // for single sidebar menu
+        $('ul.nav-sidebar a').filter(function () {
+            return this.href == url;
+        }).addClass('active');
+
+        // for sidebar menu and treeview
+        $('ul.nav-treeview a').filter(function () {
+            return this.href == url;
+        }).parentsUntil(".nav-sidebar > .nav-treeview")
+            .css({ 'display': 'block' })
+            .addClass('menu-open').prev('a')
+            .addClass('active');
+    });
+});
