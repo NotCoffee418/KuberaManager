3e2c5e1dabafeb80da5b0559227590687aa66d17
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-11-01 21:24:40 +0100

    EventLog migrations

---
+++ KuberaManager/Migrations/20201101202425_AddEventLogTables.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace KuberaManager.Migrations
+{
+    public partial class AddEventLogTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SessionId = table.Column<int>(nullable: false),
+                    TextId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventLogTexts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventLogTexts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManualSession",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ComputerId = table.Column<int>(nullable: false),
+                    SelectedScenario = table.Column<string>(nullable: true),
+                    StopManually = table.Column<bool>(nullable: false),
+                    RunUntil = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManualSession", x => x.AccountId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventLogs");
+
+            migrationBuilder.DropTable(
+                name: "EventLogTexts");
+
+            migrationBuilder.DropTable(
+                name: "ManualSession");
+        }
+    }
+}
