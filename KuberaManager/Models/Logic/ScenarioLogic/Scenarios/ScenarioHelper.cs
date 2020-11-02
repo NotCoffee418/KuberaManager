e11a5f901632cfcaca854873afc6823417f5e5ad
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-11-02 16:56:31 +0100

    Scenario selection logic implemented

--- KuberaManager/Models/Logic/ScenarioLogic/Scenarios/ScenarioHelper.cs
+++ KuberaManager/Models/Logic/ScenarioLogic/Scenarios/ScenarioHelper.cs
@@ -1,4 +1,6 @@
-﻿using KuberaManager.Models.Database;
+﻿using KuberaManager.Helpers;
+using KuberaManager.Models.Database;
+using KuberaManager.Models.Logic.ScenarioLogic.Requirements;
 using KuberaManager.Models.Logic.ScenarioLogic.Scenarios.Assigners;
 using System;
 using System.Collections.Generic;
@@ -9,9 +11,19 @@ namespace KuberaManager.Models.Logic.ScenarioLogic.Scenarios
 {
     public class ScenarioHelper
     {
-        
-#warning ensure new scenario types are added here
-        public static List<ScenarioBase> AllScenarios { 
+        /// <summary>
+        /// This s a list of scenarios bots will end up running.
+        /// If they don't meet the requirements, they'll get them first.
+        /// </summary>
+        public readonly static List<string> PrimaryScenarios = new List<string>()
+        {
+            "Quest.COOKS_ASSISTANT",
+        };
+
+
+        // WARNING!! ensure new scenario types are added here
+        public static List<ScenarioBase> AllScenarios
+        {
             get
             {
                 return new List<ScenarioBase>()
@@ -40,9 +52,44 @@ namespace KuberaManager.Models.Logic.ScenarioLogic.Scenarios
                 .FirstOrDefault();
         }
 
-        internal static ScenarioBase FindViableScenario(Account account)
+        /// <summary>
+        /// Runs through requirements and selects a random viable scenario
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public static ScenarioBase FindViableScenario(Account account)
         {
-            throw new NotImplementedException();
+            // Grab a primary scenario to run
+            if (PrimaryScenarios.Count() == 0)
+                throw new Exception("No primary scenarios are defined. Can't proceed");
+            int rand = RandomHelper.GetRandom(0, PrimaryScenarios.Count() - 1);
+            ScenarioBase baseScenario = ByIdentifier(PrimaryScenarios[rand]);
+
+            // Return scenario or child requirement to run
+            return SelectScenarioOrRequirement(baseScenario, account);
+        }
+
+        /// <summary>
+        /// This is a tree function. It will grab a random requirement or requirement of requirement of requirement...
+        /// </summary>
+        /// <param name="scen"></param>
+        /// <param name="acc"></param>
+        /// <returns></returns>
+        public static ScenarioBase SelectScenarioOrRequirement(ScenarioBase scen, Account acc)
+        {
+            // Randomize requirement order
+            List<IRequirement> randReq = scen.Requirements
+                .OrderBy(x => RandomHelper.GetRandom(0, int.MaxValue)).ToList();
+
+            // Run SelectScenarioOrRequirement on each requirement
+            // if we don't mee it, return that requirement
+            foreach (IRequirement req in randReq)
+                if (!req.DoesMeetCondition(acc))
+                    return SelectScenarioOrRequirement(req.GetFulfillScenario(acc), acc);
+
+            // We're still here, which means all requirements are met.
+            // Return the original scenario
+            return scen;
         }
     }
 }
